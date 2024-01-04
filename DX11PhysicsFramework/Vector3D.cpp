20d9eec197b2b10c969134c061e0d6bf045957fb
Harry Watts <98606501+HarryMadn3ss@users.noreply.github.com>
2024-01-04 11:05:34 +0000

    addedCrossProduct

--- DX11PhysicsFramework/Vector3D.cpp
+++ DX11PhysicsFramework/Vector3D.cpp
@@ -27,6 +27,9 @@ float Vector3D::DotProduct(Vector3D _vectorOne, Vector3D _vectorTwo)
 Vector3D Vector3D::CrossProduct(Vector3D _vectorOne, Vector3D _vectorTwo)
 {
 	Vector3D crossProduct;
+	crossProduct.x = (_vectorOne.y * _vectorTwo.z) - (_vectorOne.z * _vectorTwo.y);
+	crossProduct.y = (_vectorOne.z * _vectorTwo.x) - (_vectorOne.x * _vectorTwo.z);
+	crossProduct.z = (_vectorOne.x * _vectorTwo.y) - (_vectorOne.y * _vectorTwo.x);
 
 	return crossProduct;
 }
